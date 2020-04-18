96bf3bac848e15d8e455b13f4f42635d72051bac
Ofer <ofer.rozenbaum@gmail.com>
2020-04-18 22:56:25 +0300

    exercise

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. Feels like ' + body.current.feelslike + ' .')
+            callback(undefined, ' It is currently ' + body.current.temperature + ' degress out. Feels like ' + body.current.feelslike + '. Humidity is ' + body.current.humidity +'.')
         }
     })
 }
